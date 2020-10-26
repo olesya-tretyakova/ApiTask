e8e5b2f7380f78015ee3996cb051440bd17885d6
olesya-tretyakova <sov-olesya@yandex.ru>
2020-10-26 11:03:19 +0600

    Delete comments in build.gradle.kts

--- ApiTask/build.gradle.kts
+++ ApiTask/build.gradle.kts
@@ -20,11 +20,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.flywaydb:flyway-core") //такого нету в первом варианте
+	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("org.postgresql:postgresql") // нету в 1 варианте
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
